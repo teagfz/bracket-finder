abbc03009d3d5a92b7937a51f6aa7fc77b7f0eca
stanopoloz <stanopoloz@gmail.com>
2011-12-21 02:05:30 +0000

    Initial Release

---
+++ Bracket_Finder/Bracket_Finder/frmMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Bracket_Finder
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.RichTextBox();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtBrackets = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 280);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(116, 34);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCode.Location = new System.Drawing.Point(12, 12);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(633, 262);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.Text = "";
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(529, 280);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(116, 34);
+            this.btnPaste.TabIndex = 2;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.DarkRed;
+            this.btnSearch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSearch.Location = new System.Drawing.Point(256, 280);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(145, 34);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtBrackets
+            // 
+            this.txtBrackets.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtBrackets.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBrackets.ForeColor = System.Drawing.Color.White;
+            this.txtBrackets.Location = new System.Drawing.Point(12, 320);
+            this.txtBrackets.Name = "txtBrackets";
+            this.txtBrackets.ReadOnly = true;
+            this.txtBrackets.Size = new System.Drawing.Size(633, 157);
+            this.txtBrackets.TabIndex = 4;
+            this.txtBrackets.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(134, 280);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(116, 34);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save Log";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(407, 280);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(116, 34);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 489);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtBrackets);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnOpenFile);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bracket Finder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.RichTextBox txtCode;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox txtBrackets;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
---
+++ Bracket_Finder/Bracket_Finder/frmMain.cs
@@ -0,0 +1,157 @@
+﻿#region Headers
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+#endregion 
+
+namespace Bracket_Finder
+{
+    #region frmMain Class
+    public partial class frmMain : Form
+    {
+
+        #region Varibales 
+        
+        #region Private
+
+        private const string logpath = "logs";
+
+        #endregion
+
+        #region Public 
+        #endregion
+
+        #endregion
+
+        #region Public Functions
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private Functions
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            txtCode.Paste();
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtCode.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Please paste or open your pawn File!");
+            }
+            else 
+            {
+                if (!(txtBrackets.Text.Equals(String.Empty)))
+                {
+                    txtBrackets.Text = "";
+                }
+                SearchBraces(txtCode.Text);
+            }
+        }
+        private void SearchBraces(string code) 
+        {
+            string line;
+            int counter = 0;
+            int opbrace = 0;
+            int clbrace = 0;
+            /*int c = 0;
+            int o = 0;*/
+            using (StringReader sr = new StringReader(code)) 
+            {
+                while((line = sr.ReadLine()) != null)
+                {
+                    counter++;
+                    int index = line.IndexOf("{");
+                    int secindex = line.IndexOf("}");
+                    if (index != -1) 
+                    {
+                        opbrace++;
+                        AT("Opening Curly Barce found at line: " + counter);
+                        /*o = 2;
+                        if (!(c == 2)) 
+                        {
+                            AT("Error closing brace not found at line: " + counter);
+                        }*/
+                    }
+                    if (secindex != -1) 
+                    {
+                        clbrace++;
+                        AT("Closing Curly Barce found at line: " + counter);
+                        //c = 2;
+                    }
+                }
+            }
+            AT("\n\n\n\nTotal of " + opbrace.ToString() + " opening braces found");
+            AT("Total of " + clbrace.ToString() + " closing braces found");
+        }
+        private void SaveLog(string filename) 
+        {
+            string path = Path.Combine(Application.StartupPath, logpath);
+            if (!(Directory.Exists(path)))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else 
+            {
+                string content = txtBrackets.Text;
+                string secpath = Path.Combine(path, filename + ".txt");
+                if (File.Exists(secpath))
+                {
+                    throw new ArgumentException("Error: Log file " + filename + " already exist on the logs directory. Please remove it", "File Existance Error");
+                }
+                else 
+                {
+                    FileStream fs;
+                    fs = File.Create(secpath);
+                    fs.Close();
+                    File.WriteAllText(secpath, content);
+                }
+            }
+        }
+        private void AT(string text) 
+        {
+            txtBrackets.Text += Environment.NewLine + text;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string pathfile = IB("Please input the filename", "File Name", "Please input the filename...");
+            SaveLog(pathfile);
+        }
+        private static string IB(string prompt, string title, string defaultValue)
+        {
+            InputBox ib = new InputBox();
+            ib.FormPrompt = prompt;
+            ib.FormCaption = title;
+            ib.DefaultValue = defaultValue;
+            ib.ShowDialog();
+            string s = ib.InputResponse;
+            ib.Close();
+            return s;
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCode.Clear();
+        }
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult res;
+            ofd.Title = "Open Script";
+            ofd.Filter = "All Files|*.*";
+            res = ofd.ShowDialog();
+            if (res == DialogResult.OK) 
+            {
+                string cotent = File.ReadAllText(ofd.FileName);
+                txtCode.Clear();
+                txtCode.Text = cotent;
+            }
+        }
+        #endregion
+    }
+    #endregion
+}
